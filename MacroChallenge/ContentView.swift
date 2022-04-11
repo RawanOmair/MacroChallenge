59fcea941d732222d0d3d4b2f866da28407cca28
RawanOmair <rawain808@gmail.com>
2022-04-11 09:46:41 +0300

    Initial Commit

---
+++ MacroChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacroChallenge
+//
+//  Created by Rawan Omair on 10/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacroChallenge/MacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeApp.swift
+//  MacroChallenge
+//
+//  Created by Rawan Omair on 10/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
